70e0dd121036f2c2a85b4d54dc842933e681bc34
João Alisson <76262867+JoaoAlissonTec@users.noreply.github.com>
2023-12-22 11:28:36 -0300

    1.1.0

--- script.js
+++ script.js
@@ -6,14 +6,26 @@ let total_quantity = document.getElementById("totalquantity")
 let count = 0
 let total = 0
 let quantity_sum = 0
+
 function add() {
-    if (quantity.value != "" || product.value != "") {
+    if (quantity.value != "" && product.value != "") {
         let item = document.createElement("li")
         let item_title = document.createTextNode(quantity.value + " - " + product.value)
+        let btnExclude = document.createElement("button")
+        let btnExclude_icon = document.createElement("img")
+
+        btnExclude_icon.setAttribute("src", "./img/trash-can.png")
+
+        btnExclude.appendChild(btnExclude_icon)
+        btnExclude.setAttribute("class", "exclude-button")
+        btnExclude.setAttribute("onClick", "exclude_item("+count+")")
+
         item.appendChild(item_title)
+        item.appendChild(btnExclude)
         item.setAttribute("id", count)
         item.setAttribute("class", "item")
         list.appendChild(item)
+        
         count += 1
         total += (quantity.value * 0.5)
         quantity_sum += parseInt(quantity.value)
@@ -29,6 +41,17 @@ function exclude() {
     count = 0
     total = 0.0
     quantity_sum = 0
+    total_price.innerText = total.toFixed(2)
+    total_quantity.innerText = quantity_sum
+}
+
+function exclude_item(index){
+    let item = document.getElementById(index);
+    list.removeChild(item)
+
+    count--
+    total -= parseInt(item.innerText.substring(0,item.innerText.indexOf('-')-1)) * 0.5
+    quantity_sum -= parseInt(item.innerText.substring(0,item.innerText.indexOf('-')-1))
     total_price.innerText = total.toFixed(2)
     total_quantity.innerText = quantity_sum
 }
